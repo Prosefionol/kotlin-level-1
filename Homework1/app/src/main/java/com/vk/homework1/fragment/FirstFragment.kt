7b96f39954211d92b58148950736c74294a9ffbd
Prosefionol <ilya_kuper@mail.ru>
2022-11-18 23:49:40 +0300

    homework1 with additional assignment

--- Homework1/app/src/main/java/com/vk/homework1/fragment/FirstFragment.kt
+++ Homework1/app/src/main/java/com/vk/homework1/fragment/FirstFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -63,12 +64,19 @@ class FirstFragment : Fragment() {
 
         val but: Button = view.findViewById(R.id.activity_main__button)
         but.setOnClickListener{
-            tables.add( DataTables(
-                cnt
-            )
-            )
-            adapter.notifyDataSetChanged()
-            cnt = tables.size
+            if (tables.size > 9)
+            {
+                Toast.makeText(context, "ошибка", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                tables.add(
+                    DataTables(
+                        cnt
+                    )
+                )
+                adapter.notifyDataSetChanged()
+                cnt = tables.size
+            }
         }
 
 
